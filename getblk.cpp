ea94ae69f166151fff09786a25d17bf992b2fb1e
Rohit Kumar <kmr.rht.97@gmail.com>
2019-04-07 20:47:34 +0530

    Update getblk.cpp

--- getblk.cpp
+++ getblk.cpp
@@ -4,311 +4,102 @@
 #include<map>
 #include<condition_variable>
 #include<random>
+#include<mutex>
+#include"Buffer.h"
 
 #define total_block 50
-#define total_buffer 20 
+#define total_buffer 2
 #define num_hash 4
-#define total_thread 1
+#define total_thread 4
 
 using namespace std;
 
-condition_variable cv;
-map <int , thread::id> Lock_table;
-map <thread::id , int> req_table;
-thread::id tid;
+condition_variable par,req;
+condition_variable free_cv,hash_cv;
+mutex parm,childm;			//childm used for sync
+mutex respm,iom;			//iom used for sync
+mutex free_m,hash_m;			// rest of mutex is used for condition variable
+map <int , int> req_table;
+map <int , int> Lock_table;
+int req_thread=-1,req_t=-1,response=-1;
+int free_NULL=1; 
 
-class Buffer{
-		
-		int num;
-		int status;		//0-free,1-locked,-1-delayed write
-		int valid;		//0-invaild data, 1-vaild data
-		public:
-		Buffer *next;
-		Buffer *prev;
-		Buffer *nextfree;
-		Buffer *prevfree;
-		
-		Buffer()
-		{
-			status=0;
-			valid=0;
-			next=NULL;
-			prev=NULL;
-			nextfree=NULL;
-			prevfree=NULL;
-		}	
-		
-		Buffer(int nu,int s1=0,int s2=0,Buffer *n=NULL,Buffer *p=NULL,Buffer *nf=NULL,Buffer *pf=NULL)
-		{
-			num=nu;
-			status=s1;
-			valid=s2;
-			next=n;
-			prev=p;
-			nextfree=nf;
-			prevfree=pf;
-		}
+Buffer *response_b=NULL,*requ_b=NULL;
 
-		void setnum(int n)
-		{
-			num=n;
-		}	
+unique_lock<mutex> freel(free_m);
+unique_lock<mutex> hashl(hash_m);
 
-		void setstatus(int s)
-		{
-			status=s;
-		}
-		void setvalid(int v)
-		{
-			valid=v;
-		}
-		int getnum()
-		{
-			return num;
-		}
+void child(int process_num)
+{ 
+/*
+	Description: child thread
 	
-		int getstatus()
-		{
-			return status;
-		}
-		
-		int getvalid()
-		{
-			return valid;
-		}
-	};
-
-
-Buffer *insert_in_Hash(Buffer *Hash,Buffer *node)
-{
-	if(Hash==NULL)
-	{
-		Hash=node;
-		node->next=node;
-		node->prev=node;
-	}
-	else
-	{
-		node->prev=Hash;
-		node->next=Hash->next;
-		Hash->next=node;
-		node->next->prev=node;
-		Hash=node;
-	}
-	return Hash;
-}
-
-Buffer *insert_in_free(Buffer *free,Buffer *node)
-{
-	if(free==NULL)
-	{
-		free=node;
-		node->nextfree=node;
-		node->prevfree=node;
-	}
-	else
-	{
-		node->prevfree=free;
-		node->nextfree=free->nextfree;
-		free->nextfree=node;
-		node->nextfree->prevfree=node;
-		free=node;
-	}
-	return free;
-}
-
-Buffer *insert_in_free_begin(Buffer *free,Buffer *node)
-{
-	if(free==NULL)
-	{
-		free=node;
-		node->nextfree=node;
-		node->prevfree=node;
-	}
-	else
-	{
-		Buffer *temp;
-		temp=free->nextfree;
-		node->prevfree=temp;
-		node->nextfree=temp->nextfree;
-		temp->nextfree=node;
-		node->nextfree->prevfree=node;
-	}
-	return free;
-}
-
-void remove_from_hash(Buffer *Hashque[],int newblock,Buffer *node)
-{
-	int temp=node->getnum();
-	if(Hashque[temp%num_hash]==NULL)
-	{
-		return;
-	}
-	node->prev->next=node->next;
-	node->next->prev=node->prev;
-
-	if(Hashque[temp%num_hash]->next==Hashque[temp%num_hash])
-	{
-		Hashque[temp%num_hash]=NULL;
-	}	
-	node->setnum(newblock);
-	
-	Hashque[newblock%num_hash]=insert_in_Hash(Hashque[newblock%num_hash], node);
-}
-
-void remove_from_free(Buffer *free,Buffer *node)
-{	
-	if(free==NULL)
-	{
-		return;
-	}
-	if(free==node);
-	{
-		if(free->nextfree==free)
-		{
-			free=NULL;
-		}
-		else
-		{
-			free=free->nextfree;
-		}
-	}
+	input:
+		process_num: thread number activated
+*/
 	Buffer *temp;
-	temp=free->nextfree;
-	while( temp!=free && temp !=node)
-	{
-		temp=temp->nextfree;
-	}	
-	temp->prevfree->nextfree=temp->nextfree;
-	temp->nextfree->prevfree=temp->prevfree;
-	temp->nextfree=NULL;
-	temp->prevfree=NULL;
-}
-
-
-Buffer *search(Buffer *Hashque[],int blocknum)
-{
-	Buffer *temp,*head;
-	temp=Hashque[blocknum%num_hash];
-	head=temp;
-	if(temp->getnum()==blocknum)
-	{
-		return temp;
-	}
-	else
-	{
-		temp=temp->next;
-		while(temp!=head)
+	int input=-1;
+	while(true){
+			
+		int sat;
 		{
-			if(temp->getnum()==blocknum)
+			unique_lock<mutex> childl(childm);
+			req_t=0;
+			if(req_table[process_num]==-1)
 			{
-				return temp;
+				unique_lock<mutex> iol(iom);
+				cout<<process_num<<" requests block num :";
+				cin>>input;
+				iol.unlock();					
+				req_table[process_num]=input;
 			}
-			temp=temp->next;
-		}
-	}
-	return NULL;
-}
-
-Buffer *getblk(Buffer *Hash[],Buffer *free,int Blocknum)
-{
-	Buffer *temp,*reqBuf;
-	reqBuf=NULL;
-	while(reqBuf==NULL)
-	{
-		temp=search(Hash,Blocknum);
-		if(temp!=NULL)
-		{
-			if(temp->getstatus()==1)
-			{
-				//sleep
-				continue;
-			}
-			temp->setstatus(1);
-			remove_from_free(free,temp);
-			return temp;
-		}
-		else
-		{
-			if(free==NULL)
-			{
-				//sleep
-				continue;
-			}
-			temp=free->nextfree;
-			remove_from_free(free,temp);
-			if(temp->getstatus()==-1)
-			{
-				//sleep
-				temp->setstatus(0);
-				continue;
-			}
-			remove_from_hash(Hash,Blocknum,temp);
-			return temp;
-		}
-	}
-}
-
-Buffer *brlse(Buffer *free,Buffer *node)
-{
-	//wakeup
-	//raise execution level
-	int release=0;
-	if(node->getvalid()==1)		// and not old
-	{
-		free=insert_in_free(free,node);
-	}
-	else
-	{	
-		release=-1;
-		free=insert_in_free_begin(free,node);
-	}
-	//lower execution
-	node->setstatus(release);
-}
-
-
-
-void DisplayBuf(Buffer *Hash[],Buffer *free)
-{
-	Buffer *temp;
-	for(int i=0;i<num_hash;i++)
-	{
-		cout<<"Hash "<<i<<" :- ";
-		temp=Hash[i]->next;
-		while(temp!= Hash[i])
-		{
-			cout<<temp->getnum()<<" ";	
-			temp=temp->next;
-		}	
-		cout<<temp->getnum()<<endl;
-	}
-	cout<<endl;
-	temp=free->nextfree;
-	cout<<"free list :- ";
-	while(temp!= free)
-	{
-		cout<<temp->getnum()<<" ";
-		temp=temp->nextfree;
-	}
-	cout<<temp->getnum()<<endl;
-}
-
-void child()
-{
-	for(int i=0;i<10;i++)
-	{
-		int ran=rand()%total_block;
-		//request buffer
-		req_table[this_thread::get_id()]=ran;
-		tid=this_thread::get_id();
-		cout<<"request"<<" "<<Lock_table[ran]<<endl;
-		while(Lock_table[ran]!=this_thread::get_id())
+			req_thread=process_num;
+			sat=-1;
+			response=-1;
 			sleep(0.1);
+			unique_lock<mutex> respl(respm);
+			par.notify_one();
+			req.wait(respl, []{return response !=-1;});		
+			sat=response;
+			temp=response_b;
+			response=-1;
+			response_b=NULL;
+			childl.unlock();
+		}
+		if(sat<2)
+		{
+			if(sat==1)
+			{	
+				free_cv.wait(freel,[]{return free_NULL == 0;});
+			}
+			else
+			{
+				int temp1=req_table[process_num];
+				hash_cv.wait(hashl,[&temp1]{if(Lock_table.find(temp1)==Lock_table.end())
+									return true;
+							   else
+								return Lock_table[temp1] != 1;});
+			}
+			continue;
+		}
+		else
+		{
+			req_table[process_num]=-1;
+			sleep(rand()%5+1);	//work
+		}
 
-		//sleep((rand()%10)*0.1);	//working
-
-		
+		{
+			unique_lock<mutex> childl(childm);
+			req_t=1;
+			req_thread=process_num;
+			requ_b=temp;
+			sleep(0.1);
+			unique_lock<mutex> respl(respm);
+			par.notify_one();
+			req.wait(respl, []{return response !=-1;});	
+			childl.unlock();			
+			sleep(rand()%5+1);
+		}
 	}
 }
 
@@ -316,69 +107,109 @@ int main()
 {	
 	// Creating hash table and Free list
 	Buffer *Hash[num_hash],*free;
-	
-	for(int i=0;i<num_hash;i++)
-	{
-		Hash[i]=NULL;
-	}
-	free=NULL;
-	for(int i=0;i<total_buffer;i++)
-	{
-		Buffer *temp;
-		int hashnum=i%num_hash;
-		temp=new Buffer(i);
-		Hash[hashnum]=insert_in_Hash(Hash[hashnum],temp);
-		free=insert_in_free(free,temp);
-		Lock_table.insert(pair<int,thread::id>(temp->getnum(),0));
-	}
+	init(Hash,free,num_hash,total_buffer,Lock_table);
+	free_NULL=0;	
 
+	//creating requester threads
 	thread t[total_thread];
+	int p_status[total_thread];
+	int requested[total_thread];
 	for( int i=0;i<total_thread;i++)
 	{
-		cout<<"t"<<i<<" ";
-		req_table.insert(pair<thread::id,int>(t[i].get_id(),0));
-		t[i]=thread(child);
+		req_table.insert(pair<int,int>(i,-1));
+		p_status[i]=-1;
+		requested[i]=-1;
+		t[i]=thread(child,i);
 	}
 	sleep(0.1);
-	int f=0;
-	for(;;)
-	{
-		for( int i=0;i<total_thread;i++)
+	//main thread
+	
+	int temp;
+	while(true)
+	{	unique_lock<mutex> parl(parm);
+		par.wait(parl,[]{ return req_thread != -1;});	
+		cout<<endl;
+		requested[req_thread]=req_table[req_thread];
+		if(req_t==0)
 		{
-			if(req_table[tid]!=0)
 			{
-				Lock_table[req_table[tid]]=tid;
-				cout<<"getblock";
-				f=1;
+				unique_lock<mutex> iol(iom);
+				cout<<req_thread<<" is reqesting buffer :"<<req_table[req_thread]<<endl;
+				iol.unlock();
+			}
+			response_b=getblk(Hash,free,req_table[req_thread],Lock_table);
+			if(free==NULL)
+			{
+				free_NULL=1;
+			}
+			if(response_b==NULL)
+			{
+				if(Lock_table.find(req_table[req_thread])==Lock_table.end())
+				{
+					p_status[req_thread]=1;
+					response=1;
+				}
+				else
+				{
+					p_status[req_thread]=0;
+					response=0;
+				}
+			}
+			else
+			{
+				p_status[req_thread]=2;
+				response=2;
 			}
 		}
-		if(f==1)
-			break;
-	}
-	
-	Buffer *temp1,*temp2,*temp3;
-
-	temp1=getblk(Hash,free,23);
-	
-	DisplayBuf(Hash,free);
-
-	
-	temp2=getblk(Hash,free,12);
-	
-	DisplayBuf(Hash,free);
-
-	brlse(free,temp1);	
-
-	temp3=getblk(Hash,free,24);
-	
-	DisplayBuf(Hash,free);
-
-	brlse(free,temp2);
-	brlse(free,temp3);
-	
-	DisplayBuf(Hash,free);
-
-	t[0].join();	
+		else
+		{
+			{
+				unique_lock<mutex> iol(iom);
+				cout<<req_thread<<" is releasing buffer: "<<requ_b->getnum()<<endl;
+				iol.unlock();			
+			}	
+			free=brlse(free,requ_b,Lock_table);
+			free_NULL=0;
+			p_status[req_thread]=-1;
+			free_cv.notify_all();
+			hash_cv.notify_all();
+			response=0;
+		}	
+		temp=req_thread;
+		req_thread=-1;
+		sleep(0.1);
+		req.notify_one();
+		unique_lock<mutex> iol(iom);
+		DisplayBuf(Hash,free);
+		cout<<"Buffer status ( -1 for delayed write, 1 for locked and 0 for free) :"<<endl;
+		for(map<int, int>::iterator it=Lock_table.begin();it!=Lock_table.end();it++)
+		{
+			cout<<"("<<it->first<<","<<it->second<<")  ";
+		}
+		cout<<endl;
+		cout<<"Process status :"<<endl;
+		for(int i=0;i<total_thread;i++)
+		{
+			cout<<"("<<i;
+			if(p_status[i]==2)
+				cout<<" Locked "<<requested[i];
+			else
+			{
+				if(p_status[i]==1)
+					cout<<" Waiting for Free";
+				else
+				{
+					if(p_status[i]==0)
+						cout<<" Waiting for "<<requested[i];
+					else
+						cout<<" Waiting to request";
+				}
+			}
+			cout<<")  ";
+		}
+		cout<<endl<<endl<<endl;
+		iol.unlock();
+	}	
 	
 	return 1;
 }
